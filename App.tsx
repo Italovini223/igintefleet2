c2f3167cd3301f6cf0db4d78721fd1742173964c
Italovini223 <italovinicius2018@gmail.com>
2023-11-21 19:58:11 -0300

    ✨ feat: add fonts and Load Component

--- App.tsx
+++ App.tsx
@@ -1,9 +1,23 @@
-import { View } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+import { } from 'react-native'
+
+import { ThemeProvider} from 'styled-components/native'
+import theme from './src/theme';
 
 import { SingIn } from './src/screens/SingIn';
+import { Loading } from './src/components/Loading';
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
+
+  if(!fontsLoaded){
+    return(
+      <Loading />
+    )
+  }
   return (
-  <SingIn />
+    <ThemeProvider theme={theme}>
+      <SingIn />
+    </ThemeProvider>
   );
 }
 
