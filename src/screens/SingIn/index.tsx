1a9cf6ae80433c282cfc4435fb66bd4ea143123d
Italovini223 <italovinicius2018@gmail.com>
2023-11-21 19:36:28 -0300

    add files

---
+++ src/screens/SingIn/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text, StatusBar } from 'react-native';
+
+import { styles } from './styles';
+
+export function SingIn() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar />
+    </View>
+  );
+}
