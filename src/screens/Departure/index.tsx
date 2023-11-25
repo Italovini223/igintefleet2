bd34192be3a7d2495c0cb98d87457c8afe187590
Italovini223 <italovinicius2018@gmail.com>
2023-11-25 17:02:30 -0300

    💄 add scroll

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,13 +1,16 @@
-import { useRef } from 'react';
-import { TextInput } from 'react-native';
-import { Button } from '../../components/Button';
-import { Header } from '../../components/Header';
-import { LicensePlateInput } from '../../components/LicensePlateInput';
-import { TextAreaInput } from '../../components/TextAreaInput';
-import { Container, Content } from './styles';
+import { useRef } from 'react'
+import { TextInput, ScrollView, KeyboardAvoidingView, Platform } from 'react-native'
+
+import { Button } from '../../components/Button'
+import { Header } from '../../components/Header'
+import { LicensePlateInput } from '../../components/LicensePlateInput'
+import { TextAreaInput } from '../../components/TextAreaInput'
+
+import { Container, Content } from './styles'
 
 export function Departure() {
   const descriptionRef = useRef<TextInput>(null)
+  const keyboardAvoidingViewBehavior = Platform.OS === 'android' ? 'height' : 'position'
 
   function handleDepartureRegister(){
     console.log("ok!");
@@ -16,29 +19,33 @@ export function Departure() {
   return (
     <Container>
       <Header title='Saida'/>
-      
-      <Content>
-        <LicensePlateInput 
-          label='Placa do veiculo'
-          placeholder='BRA1234'
-          onSubmitEditing={() => descriptionRef.current?.focus()}
-          returnKeyType='next'
-        /> 
 
-        <TextAreaInput 
-          ref={descriptionRef}
-          label='Finalidade'
-          placeholder='Vou utilizar o veiculo para...' 
-          onSubmitEditing={handleDepartureRegister}
-          returnKeyType='send'
-          blurOnSubmit
-        />
+      <KeyboardAvoidingView style={{ flex: 1 }} behavior={keyboardAvoidingViewBehavior}>
+        <ScrollView>
+          <Content>
+            <LicensePlateInput 
+              label='Placa do veiculo'
+              placeholder='BRA1234'
+              onSubmitEditing={() => descriptionRef.current?.focus()}
+              returnKeyType='next'
+            /> 
 
-        <Button 
-          title='Registrar saída'
-          onPress={handleDepartureRegister}
-        />
-      </Content>
+            <TextAreaInput 
+              ref={descriptionRef}
+              label='Finalidade'
+              placeholder='Vou utilizar o veiculo para...' 
+              onSubmitEditing={handleDepartureRegister}
+              returnKeyType='send'
+              blurOnSubmit
+            />
+
+            <Button 
+              title='Registrar saída'
+              onPress={handleDepartureRegister}
+            />
+          </Content>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
