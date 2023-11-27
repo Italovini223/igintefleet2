00011167a19dbc511783c9979a8e0869429b3bb1
Italovini223 <italovinicius2018@gmail.com>
2023-11-27 17:51:21 -0300

    ✨ feat: get vehicle details

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,12 +1,16 @@
-import { useRoute } from '@react-navigation/native';
+import { useRoute } from '@react-navigation/native'
 
-import { X } from 'phosphor-react-native';
+import { X } from 'phosphor-react-native'
 
-import { Header } from '../../components/Header';
-import { Button } from '../../components/Button';
-import { ButtonIcon } from '../../components/ButtonIcon';
+import { useObject } from '../../libs/realm'
+import { Historic } from '../../libs/realm/schemas/Historic'
+
+import { Header } from '../../components/Header'
+import { Button } from '../../components/Button'
+import { ButtonIcon } from '../../components/ButtonIcon'
 
 import { Container, Content, Description, Footer, Label, LicensePlate } from './styles'
+import { BSON } from 'realm'
 
  type RoutePramsProps = {
   id: string;
@@ -15,6 +19,8 @@ import { Container, Content, Description, Footer, Label, LicensePlate } from './
 export function Arrival() {
   const route = useRoute()
   const { id } = route.params as RoutePramsProps;
+
+  const historic = useObject(Historic, new BSON.UUID(id) as unknown as string)
 
   return (
     <Container>
@@ -27,7 +33,7 @@ export function Arrival() {
         </Label>
 
         <LicensePlate>
-          PPP0000
+          {historic?.license_plate}
         </LicensePlate>
 
         <Label>
@@ -35,7 +41,7 @@ export function Arrival() {
         </Label>
 
         <Description>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, ipsam. Placeat distinctio expedita voluptatibus, aspernatur iure sit sequi corporis atque facere illo natus? Officia delectus doloremque laudantium sit tempore animi?
+          {historic?.description}
         </Description>
         <Footer>
           <ButtonIcon 
