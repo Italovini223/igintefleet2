e9608c8d4388d411d0063ad4836c6e3d4bd8795b
Italovini223 <italovinicius2018@gmail.com>
2023-11-27 17:43:34 -0300

    💄created  ButtonIcon

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,8 +1,12 @@
-
 import { useRoute } from '@react-navigation/native';
-import { Container, Content, Description, Footer, Label, LicensePlate } from './styles';
+
+import { X } from 'phosphor-react-native';
+
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
+import { ButtonIcon } from '../../components/ButtonIcon';
+
+import { Container, Content, Description, Footer, Label, LicensePlate } from './styles'
 
  type RoutePramsProps = {
   id: string;
@@ -34,6 +38,9 @@ export function Arrival() {
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, ipsam. Placeat distinctio expedita voluptatibus, aspernatur iure sit sequi corporis atque facere illo natus? Officia delectus doloremque laudantium sit tempore animi?
         </Description>
         <Footer>
+          <ButtonIcon 
+            icon={X}
+          />
           <Button 
             title='Registar chegada'
           />
