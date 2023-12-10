2d35925e1ec66b1e550b4112461f88a0a46531d0
Italovini223 <italovinicius2018@gmail.com>
2023-12-10 11:38:09 -0300

    ✨ progress notification

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -68,6 +68,12 @@ export function Home() {
     navigate('arrival', { id });
   }
 
+  function progressNotification(transferred: number, transferable: number){
+    const percentage = (transferred/transferable) * 100;
+
+    console.log("TRANSFERIDO => ", percentage)
+  }
+
   useEffect(() => {
     fetchVehicleInUse()
   }, [])
@@ -94,6 +100,22 @@ export function Home() {
       mutableSubs.add(historicByUserQuery, { name: 'historic_by_user'});
     })
   }, [realm]);
+
+  useEffect(() => {
+    const syncSession = realm.syncSession;
+
+    if(!syncSession){
+      return;
+    }
+
+    syncSession.addProgressNotification(
+      Realm.ProgressDirection.Upload,
+      Realm.ProgressMode.ReportIndefinitely,
+      progressNotification
+    )
+    
+    return () => syncSession.removeProgressNotification(progressNotification)
+  }, []);
 
   return (
     <Container>
