4c3ed91947033f6b8c53d7dd7690097b35b7f27c
Italovini223 <italovinicius2018@gmail.com>
2024-01-08 17:58:14 -0300

    ✨ feat: Adjusting the map to display markers

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,6 +1,8 @@
+import { useRef } from 'react';
+
 import MapView, { PROVIDER_GOOGLE, MapViewProps,  LatLng, Marker} from 'react-native-maps';
 
-import { Car, FlagCheckered } from 'phosphor-react-native'
+import { Car, FlagCheckered } from 'phosphor-react-native';
 import { IconBox } from '../IconBox';
 
 type Props = MapViewProps & {
@@ -10,8 +12,19 @@ type Props = MapViewProps & {
 export function Map({ coordinates, ...rest}: Props){
 
   const lastCoordinate = coordinates[coordinates.length - 1];
+  const mapRef = useRef<MapView>(null);
+
+  async function onMapLoaded(){
+    if(coordinates.length > 1){
+      mapRef.current?.fitToSuppliedMarkers(['departure', 'arrival'], {
+        edgePadding: { top: 50, right: 50, bottom: 50, left: 50}
+      })
+    }
+  }
+
   return(
    <MapView 
+   ref={mapRef}
     provider={PROVIDER_GOOGLE}
     style={{
       width: '100%',
@@ -23,15 +36,16 @@ export function Map({ coordinates, ...rest}: Props){
       latitudeDelta: 0.005,
       longitudeDelta: 0.005
     }}
+    onMapLoaded={onMapLoaded}
     {...rest}
    >
-    <Marker coordinate={coordinates[0]} >
+    <Marker identifier='departure' coordinate={coordinates[0]} >
       <IconBox size='SMALL' icon={Car}/>
     </Marker>
     
     {
       coordinates.length > 1 &&
-      <Marker coordinate={lastCoordinate}>
+      <Marker identifier='arrival' coordinate={lastCoordinate}>
         <IconBox size='SMALL' icon={FlagCheckered}/>
       </Marker>
     }
