7bb9e671fb4bd7675cce9e294dd200404222e77d
Italovini223 <italovinicius2018@gmail.com>
2024-01-08 14:14:35 -0300

    🔧 using api key

--- app.config.js
+++ app.config.js
@@ -1,4 +1,8 @@
-{
+ import * as dotenv from 'dotenv';
+
+ dotenv.config();
+ 
+ module.exports = {
   "expo": {
     "name": "igintefleet2",
     "slug": "igintefleet2",
@@ -24,14 +28,22 @@
       ]
     ],
     "ios": {
-      "supportsTablet": true
+      "supportsTablet": true,
+      "config": {
+        "googleMapsApiKey": process.env.GOOGLE_MAPS_API_KEY
+      }
     },
     "android": {
       "adaptiveIcon": {
         "foregroundImage": "./assets/adaptive-icon.png",
         "backgroundColor": "#202024"
       },
-      "package": "com.italobessa.ignitefleet2"
+      "package": "com.italobessa.ignitefleet2",
+      "config": {
+        "googleMaps": {
+          "apiKey": process.env.GOOGLE_MAPS_API_KEY
+        }
+      }
     },
     "web": {
       "favicon": "./assets/favicon.png"
